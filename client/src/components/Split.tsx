1184181c57d2037ac2a2bad7f318fe8ba5749a9e
Gabriel Han <gabrielhan104@gmail.com>
2022-10-16 06:17:40 -0700

    hi

---
+++ client/src/components/Split.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import callOCRAPI from '../api/recognize';
+
+import '../styles/split.css';
+
+function Split() {
+  return (
+    <section className="title">
+      <h1>Join Party</h1>
+    </section>
+  );
+}
+
+export default Split;
